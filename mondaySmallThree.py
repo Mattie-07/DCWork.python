3cbaeb297434abbac09dada40b52982c4bc2f942
Matthew <Matthewtroberts10@gmail.com>
2020-11-16 23:13:20 -0600

    All of the homework from Monday plus class notes

---
+++ mondaySmallThree.py
@@ -0,0 +1,9 @@
+#3. Friend counter
+# Using the dictionary from the Nested dictionaries exercise above, write a function countFriends() that accepts
+#  a dictionary as an argument and returns a new dictionary that includes a new key
+
+
+def countFriends(newDictionary):
+    if newDictionary !=type(list)
+        print("Sorry. You can't use a dictonary or list to create a new key")
+    return newDictionary = {}
---
+++ mondaySmallTwo.py
@@ -0,0 +1,63 @@
+# 2. Nested dictionaries
+# Given the following dictionary:
+
+# ramit = {
+#   'name': 'Ramit',
+#   'email': 'ramit@gmail.com',
+#   'interests': ['movies', 'tennis'],
+#   'friends': [
+#     {
+#       'name': 'Jasmine',
+#       'email': 'jasmine@yahoo.com',
+#       'interests': ['photography', 'tennis']
+#     },
+#     {
+#       'name': 'Jan',
+#       'email': 'jan@hotmail.com',
+#       'interests': ['movies', 'tv']
+#     }
+#   ]
+# }
+
+ramit = {
+    'name' : 'Ramit',
+    'email': 'ramit@gmail.com',
+    'interests' : ['movies', 'tennis'],
+    'friends': [
+        {
+            'name': 'Jasmine',
+            'email': 'Jasmine@yahoo.com',
+            'interests' : ['photography', 'tennis']
+        },
+        {
+            'name': 'Jan',
+            'email': 'Jan@hotmail.com',
+            'interests': ['movies', 'tv']
+        }
+    ]
+}
+
+
+email = ramit["email"]
+emailName = email
+print(f"Ramti's email is {emailName}")
+
+interest = ramit["interests"][0]
+firstInterest = interest
+print(f"Ramit's first interest is {interest}")
+
+
+friend = ramit["friends"]
+jazEmail = friend[0]
+jaz = jazEmail["email"]
+print(jaz)
+
+jan =  friend[1]
+moviesJan = jan["interests"][0]
+print(moviesJan)
+
+
+# Write a python expression that gets the email address of Ramit.
+# Write a python expression that gets the first of Ramit's interests.
+# Write a python expression that gets the email address of Jasmine.
+# Write a python expression that gets the second of Jan's two interests
